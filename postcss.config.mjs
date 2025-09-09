16e148b2a0608bf410d3720aad1adccb89c6c8a3
prince sunsara <princesunasara977@gmail.com>
2025-09-09 17:01:54 +0530

    some adjustment

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,6 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {},
+    autoprefixer: {}, // keep this, Next 15 expects it
+  },
 };
-
-export default config;
